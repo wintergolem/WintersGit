dafbf6d26cdcbd42b289740f141e161aa20dff5e
wintergolem <wintergolem@gmail.com>
2013-09-25 20:41:33 -0700

    Added ProjectShaderShell

---
+++ Projects/Shader_Project_Shell/Shader_Project_Shell/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Shader_Project_Shell.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
